31e8965f8f7b10e69ca0c792fc93684da2b91f73
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2020-06-20 17:58:49 +0200

    Added Caller class.

---
+++ src/bt/reflect/methods/Caller.java
@@ -0,0 +1,158 @@
+package bt.reflect.methods;
+
+import java.lang.StackWalker.Option;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+
+/**
+ * @author &#8904
+ *
+ */
+public class Caller
+{
+    public static String formatParameterValues(Object... values)
+    {
+        String ret = "";
+
+        var stack = StackWalker.getInstance(Option.RETAIN_CLASS_REFERENCE)
+                               .walk(s -> s.skip(1)
+                                           .findFirst())
+                               .get();
+
+        Class cls = stack.getDeclaringClass();
+        String methodName = stack.getMethodName();
+        MethodType type = stack.getMethodType();
+
+        try
+        {
+            Method m = cls.getMethod(methodName, type.parameterArray());
+
+            Parameter p;
+
+            for (int i = 0; i < m.getParameters().length; i ++ )
+            {
+                p = m.getParameters()[i];
+                ret += "[" + p.getName() + " = ";
+
+                if (i < values.length)
+                {
+                    ret += valueToString(values[i]);
+                }
+
+                ret += "]";
+            }
+        }
+        catch (NoSuchMethodException | SecurityException e)
+        {
+            e.printStackTrace();
+        }
+
+        return ret;
+    }
+
+    public static String formatCallerString(Object... values)
+    {
+        var stack = StackWalker.getInstance(Option.RETAIN_CLASS_REFERENCE)
+                               .walk(s -> s.skip(1)
+                                           .findFirst())
+                               .get();
+
+        String className = stack.getClassName();
+        String methodName = stack.getMethodName();
+        MethodType type = stack.getMethodType();
+
+        var str = new StringBuilder();
+        str.append(className);
+        str.append(".");
+        str.append(methodName);
+        str.append("(");
+
+        Class param;
+
+        for (int i = 0; i < type.parameterArray().length; i ++ )
+        {
+            param = type.parameterArray()[i];
+            str.append(param.getSimpleName());
+
+            if (i < values.length)
+            {
+                str.append(" = " + valueToString(values[i]));
+            }
+
+            str.append(", ");
+        }
+
+        String ret = str.toString();
+
+        if (type.parameterCount() > 0)
+        {
+            ret = ret.substring(0, ret.length() - 2);
+        }
+        str.setLength(0);
+        str.append(ret);
+        str.append(") : ");
+        str.append(stack.getLineNumber());
+
+        return str.toString();
+    }
+
+    protected static String valueToString(Object value)
+    {
+        String ret = "";
+
+        if (value != null)
+        {
+            if (value.getClass().isArray())
+            {
+                if (value.getClass() == boolean[].class)
+                {
+                    ret += Arrays.toString((boolean[])value);
+                }
+                else if (value.getClass() == byte[].class)
+                {
+                    ret += Arrays.toString((byte[])value);
+                }
+                else if (value.getClass() == short[].class)
+                {
+                    ret += Arrays.toString((short[])value);
+                }
+                else if (value.getClass() == int[].class)
+                {
+                    ret += Arrays.toString((int[])value);
+                }
+                else if (value.getClass() == long[].class)
+                {
+                    ret += Arrays.toString((long[])value);
+                }
+                else if (value.getClass() == float[].class)
+                {
+                    ret += Arrays.toString((float[])value);
+                }
+                else if (value.getClass() == double[].class)
+                {
+                    ret += Arrays.toString((double[])value);
+                }
+                else if (value.getClass() == char[].class)
+                {
+                    ret += Arrays.toString((char[])value);
+                }
+                else
+                {
+                    ret += Arrays.toString((Object[])value);
+                }
+            }
+            else
+            {
+                ret += value;
+            }
+        }
+        else
+        {
+            ret = "null";
+        }
+
+        return ret;
+    }
+}
